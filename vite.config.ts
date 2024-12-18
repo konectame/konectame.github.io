9ee12fbd23136e20aa95b8015341d46b3ab0e145
Alexander A. Ramírez M <soyalexanderramirez@gmail.com>
2024-12-18 10:15:58 -0400

    deploy to docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  },
   plugins: [react()],
   resolve: {
     alias: {
